9c2ed25fa08103e0dd74173871d41ce51e3456cd
Haz <me@harrysy.red>
2021-11-03 15:51:08 +0000

    Making it use pwd/mysql.php

--- public_html/scripts/db.php
+++ public_html/scripts/db.php
@@ -1,12 +1,13 @@
 <?php
-$sqlservername = "localhost";
-$sqlusername = "passroot";
-$sqlpassword = "Genetics-Rockband-Radiated-Ahead";
-$sqldbname = "passman";
+require "../pwd/mysql.php"; // grabbing sql loggins from one place
+//$sqlservername = "localhost";
+// $sqlusername = "passroot";
+// $sqlpassword = "Genetics-Rockband-Radiated-Ahead";
+// $sqldbname = "passman";
 
 // Create connection
-$conn = mysqli_connect($sqlservername, $sqlusername, $sqlpassword, $sqldbname);
-
+// $conn = mysqli_connect($sqlservername, $sqlusername, $sqlpassword, $sqldbname);
+$conn = mysqli_connect($sqlHost,$sqlUsername,$sqlPassword,$sqlDatabase)// using centeral logins
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 }
