e21e727c5080aaeb5946be6243ccd3cc864afaf0
Haz <me@harrysy.red>
2021-11-04 14:52:22 +0000

    Update csvUpload.php

--- public_html/scripts/csvUpload.php
+++ public_html/scripts/csvUpload.php
@@ -1,8 +1,9 @@
 
 
 <?php
+
 if(isset($_POST["submit"])) {
-    print_r($_FILES["fileTOUpload"]);
+    print_r($_FILES["fileToUpload"]);
 }else{
     echo('<form action="csvUpload.php" method="post" enctype="multipart/form-data">    Select file to upload:    <input type="file" name="fileToUpload" id="fileToUpload">    <input type="submit" value="Upload File" name="submit"></form>');
 }
