25f31f397328a7f440ae4f0c6ce3c110b5750ead
Group 8 <group8@harrysy.red>
2021-11-04 15:45:50 +0000

    Some simple changes as computer died.

--- public_html/scripts/csvUpload.php
+++ public_html/scripts/csvUpload.php
@@ -3,8 +3,13 @@
 <?php
 
 if(isset($_POST["submit"])) {
-    print_r($_FILES["fileToUpload"]);
+	//print_r($_FILES["fileToUpload"]);
+	if(($_FILES["fileToUpload"]["type"] == "text/csv") AND ($_FILES["fileToUpload"]["size"] <= "200")){
+		$lines = file($_FILES["fileToUpload"]["tmp_name"]);
+		echo "File ok";
+		//echo sizeof($lines);
+	}
 }else{
-    echo('<form action="csvUpload.php" method="post" enctype="multipart/form-data">    Select file to upload:    <input type="file" name="fileToUpload" id="fileToUpload">    <input type="submit" value="Upload File" name="submit"></form>');
+	echo('<form action="csvUpload.php" method="post" enctype="multipart/form-data">    Select file to upload:    <input type="file" name="fileToUpload" id="fileToUpload">    <input type="submit" value="Upload File" name="submit"></form>');
 }
 ?>
