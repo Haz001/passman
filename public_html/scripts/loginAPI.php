6e55e3ff96b3106d275e43e1a5732a209568ad01
BenForino <benforino@gmail.com>
2021-11-04 16:15:39 +0000

    fixing harrys bugs

--- public_html/scripts/loginAPI.php
+++ public_html/scripts/loginAPI.php
@@ -6,13 +6,13 @@ $password = "";
 // require './hash.php';
 require "../pwd/mysql.php";
 $dsn = "mysql:host=$sqlHost;dbname=$sqlDatabase";
-$sqlConn = new PDO($dsn, $sqlUsername, $sqlPassword, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::MYSQL_ATTR_SSL_CAPATH => '/public_html', PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => 0));
+$sqlConn = new PDO($dsn, $sqlUsername, $sqlPassword);
 $sql = "SELECT * FROM users WHERE username = :username";
 $statement = $sqlConn->prepare($sql);
 $statement->bindParam(':username', $username, PDO::PARAM_STR);
 $statement->execute();
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-$passwordhash = password_hash($password+$username,,);
+$passwordhash = password_hash($password, $username);
 if (count($rows) == 1)
 {
 	if (
@@ -36,4 +36,3 @@ else
 //elseif (($action == "search") and $debug) {
 //	echo "Result:<pre>".htmlspecialchars(var_export($rows,true))."</pre>";
 //	//print_r(str_replace("\n","<br/>",var_export($rows,true)));
-}?>
