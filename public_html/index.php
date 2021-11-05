025ff1bdbde1aae59a610c173167805feb6f5320
BenForino <benforino@gmail.com>
2021-11-05 12:09:06 +0000

    Setting up signup and login

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?php require_once "header.php"; ?>
+
+<h1>Pass Man</h1>
+<p>
+	The De Facto password manager
+
+<ul>
+	<li><a href="login.php">Login</a></li>
+	<li><a href="signup.php">Signup</a></li>
+</ul>
+</p>
+</body>
+
+</html>
