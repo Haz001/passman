bbd9664eba393bdf096899537f5f53da3279a128
BenForino <benforino@gmail.com>
2021-11-05 12:08:45 +0000

    Adding Header

---
+++ public_html/header.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/styles/styles.css">
+    <title>PassMan</title>
+</head>
+
+<body>
