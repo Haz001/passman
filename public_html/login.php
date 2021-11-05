1965f5b2e92ff9374b6cd39511ff5ab03b22bfe7
Haz <haz001@hazonline.co.uk>
2021-11-05 21:08:39 +0000

    testing login page and removing whitespace

--- public_html/login.php
+++ public_html/login.php
@@ -1,1 +1,32 @@
-<?php require_once "header.php";
+<?php require_once "header.php";?>
+<?php
+$state = 0;
+echo $_SERVER['REQUEST_METHOD'];
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+	$state = 0;
+}
+elseif ($_SERVER['REQUEST_METHOD'] == "POST")
+{
+
+}
+$page = [];
+if($state == 0){
+	$page["title"] => "Sign In";
+	$page["subTitle"] =>"Use your PassMan Account";
+	$page["label"] =>"Username";
+	$page["inputType"] => "text";
+	$page["inputName"] => "username"
+}
+ ?>
+<div>
+
+	<form action="login.php">
+		<div>
+			<img src="logo.png"/> <span>PassMan</span>
+		</div>
+		<span class="titel"><?php print($title)?></span>
+		<span class="subTitle"><?php print($subtitle)?></span>
+
+	</form>
+</div>
+<?php require_once "footer.php";?>
