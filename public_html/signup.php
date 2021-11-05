7a44b8941827d023ffb30ac0e3d1437951d82b4d
Ben <benforino@gmail.com>
2021-11-05 20:14:38 +0000

    signup form add

--- public_html/signup.php
+++ public_html/signup.php
@@ -1,1 +1,12 @@
-<?php require_once "header.php";
+<?php require_once "header.php"; ?>
+
+<form action="scripts/signupScript.php" method="POST">
+    <input type="text" name="first_name" placeholder="First Name">
+    <input type="text" name="last_name" placeholder="Last Name">
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="dob" placeholder="Date of Birth">
+    <input type="text" name="mobile" placeholder="Mobile Number">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit" name="submit">Submit</button>
+</form>
