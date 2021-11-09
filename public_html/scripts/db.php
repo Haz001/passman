34cf5764856e073399e97f92aa61291f74e1bb1a
BenForino <benforino@gmail.com>
2021-11-09 12:24:43 +0000

    minor changes

--- public_html/scripts/db.php
+++ public_html/scripts/db.php
@@ -7,7 +7,7 @@ require "../pwd/mysql.php"; // grabbing sql loggins from one place
 
 // Create connection
 // $conn = mysqli_connect($sqlservername, $sqlusername, $sqlpassword, $sqldbname);
-$conn = mysqli_connect($sqlHost,$sqlUsername,$sqlPassword,$sqlDatabase);// using centeral logins
+$conn = mysqli_connect($sqlHost, $sqlUsername, $sqlPassword, $sqlDatabase); // using centeral logins
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 }
