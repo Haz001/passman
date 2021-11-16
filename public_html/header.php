cc938263051d9af777268ea4f92a1c7d38434b4e
oskar9890 <oskarpapaj22@gmail.com>
2021-11-16 12:57:53 +0000

    css

--- public_html/header.php
+++ public_html/header.php
@@ -12,8 +12,20 @@ session_start(["cookie_domain" => "passman.harrysy.red"]);
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="scripts/errorHandle.js"></script>
 	<title>PassMan</title>
-	<h1 class="headerTitle">PassMan</h1>
-	<h3 class="headerMessage">Hello sam</h3>
-	<img class="headerImage" src="/img/profilePicture.jpeg" alt="profile pic">
+</head>
+
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/styles/styles.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="scripts/errorHandle.js"></script>
+	<title>PassMan</title>
+	<div class="navbar">
+		<h1 class="headerTitle">PassMan</h1>
+		<h3 class="headerMessage">Hello sam</h3>
+		<img class="headerImage" src="/img/profilePicture.jpeg" alt="profile pic">
+	</div>
 </head>
 
