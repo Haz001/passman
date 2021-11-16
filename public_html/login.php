9e79f5ecc6fd4e8f4dcfb68ba231afb6ae75cab3
Khal-R <91947633+Khal-R@users.noreply.github.com>
2021-11-16 12:45:35 +0000

    Update

--- public_html/login.php
+++ public_html/login.php
@@ -1,9 +1,20 @@
-<?php require_once "header.php";
-?>
+<link rel="stylesheet" href="/styles/styles.css">
 
-<form action="scripts/loginScript.php" method="POST">
-	<input type="text" name="username" placeholder="Username/Email">
-	<input type="password" name="password" placeholder="Password">
-	<button type="submit" name="submit">Submit</button>
-</form>
-<?php require "footer.php"; ?>
+<div class="containerMain">
+	<div class="containerMainLeft">
+		<div>
+			<img src="logo.png"/><span>Pass</span>Man
+		</div>
+	</div>
+	<div class="containerMainLeft">
+		<form action="scripts/loginScript.php" method="POST">
+			<input type="text" name="username" placeholder="Username/Email">
+			<input type="password" name="password" placeholder="Password">
+			<button type="submit" name="submit">Submit</button>
+		</form>
+		<div>
+			<p id="error"></p>
+		</div>
+	</div>
+
+</div>
