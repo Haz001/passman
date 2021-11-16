b22292baa0e81087109fcdac8617f636509222da
BenForino <benforino@gmail.com>
2021-11-16 12:50:38 +0000

    login.php

--- public_html/login.php
+++ public_html/login.php
@@ -1,20 +1,35 @@
-<link rel="stylesheet" href="/styles/styles.css">
+<?php
+session_start(["cookie_domain" => "passman.harrysy.red"]);
+?>
 
-<div class="containerMain">
-	<div class="containerMainLeft">
-		<div>
-			<img src="logo.png"/><span>Pass</span>Man
-		</div>
-	</div>
-	<div class="containerMainLeft">
-		<form action="scripts/loginScript.php" method="POST">
-			<input type="text" name="username" placeholder="Username/Email">
-			<input type="password" name="password" placeholder="Password">
-			<button type="submit" name="submit">Submit</button>
-		</form>
-		<div>
-			<p id="error"></p>
-		</div>
-	</div>
+<!DOCTYPE html>
+<html lang="en">
 
-</div>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/styles/styles.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="scripts/errorHandle.js"></script>
+	<title>PassMan</title>
+</head>
+
+<body>
+	<div class="containerMain">
+		<div class="containerMainLeft">
+			<div>
+				<img src="logo.png" /><span>Pass</span>Man
+			</div>
+		</div>
+		<div class="containerMainLeft">
+			<form action="scripts/loginScript.php" method="POST">
+				<input type="text" name="username" placeholder="Username/Email">
+				<input type="password" name="password" placeholder="Password">
+				<button type="submit" name="submit">Submit</button>
+			</form>
+			<div>
+				<p id="error"></p>
+			</div>
+		</div>
+
+	</div>
