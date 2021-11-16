499394e6e45e7265ccfb09d1c011c4a7756bb50d
BenForino <benforino@gmail.com>
2021-11-16 13:55:14 +0000

    css

--- public_html/scripts/errorHandle.js
+++ public_html/scripts/errorHandle.js
@@ -9,36 +9,44 @@ $(document).ready(function () {
 });
 
 function errorMsg($_GET) {
-	if ($_GET["error"] !== "") {
-		$("#error").css("background", "lightcoral");
-		$("#error").css("padding", "10px");
-	}
 	switch ($_GET["error"]) {
 		case "ef":
+			$("#error").css("background", "lightcoral");
+			$("#error").css("padding", "10px");
 			$("#error").html(
 				"There are empty fields, please check your details and try again"
 			);
 			break;
 		case "notfound":
+			$("#error").css("background", "lightcoral");
+			$("#error").css("padding", "10px");
 			$("input").css("border-color", "red");
 			$("#error").html(
 				"Username/Password inccorect, please check your details and try again"
 			);
 			break;
 		case "stmtfailed":
+			$("#error").css("background", "lightcoral");
+			$("#error").css("padding", "10px");
 			$("#error").html(
 				"Database error, please try again in a few minutes, if issue persists, contact support"
 			);
 			break;
 		case "otpExpired":
+			$("#error").css("background", "lightcoral");
+			$("#error").css("padding", "10px");
 			$("#error").html(
 				"Your one time passcode has expired, please login again, otp's expire after 20 minutes"
 			);
 			break;
 		case "maxlength":
+			$("#error").css("background", "lightcoral");
+			$("#error").css("padding", "10px");
 			$("#error").html("Max input length exceeded");
 			break;
 		case "complexity":
+			$("#error").css("background", "lightcoral");
+			$("#error").css("padding", "10px");
 			$("#error").html(
 				'Password does not meet complexity requirements <br> <ul style="list-style-type: none"><li>8 Characters</li><li>1 Special Character</li><li>1 Uppercase Character</li><li>1 Number</li></ul>'
 			);
