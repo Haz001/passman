c708b93a1f57aaef815d0cbcaf163f418c7ffccf
BenForino <benforino@gmail.com>
2021-11-17 15:59:22 +0000

    ajax fix

--- public_html/menu.php
+++ public_html/menu.php
@@ -1,25 +1,35 @@
 <?php require_once "header.php";
-//?>
-    <div id="list"><button onclick="listWebsites(this.parent)" >Refresh</button> </div>
+//
+?>
+<div id="list"><button onclick="listWebsites(this.parent)">Refresh</button> </div>
 <script>
-    function listWebsites(result){
-        if(result.status == 200) {
+    function listWebsites(result) {
+        if (result.status == 200) {
             let websitesJson = result.responseText;
             let websites = JSON.parse(websitesJson);
             console.log(websites);
-            for (let i = 0;i < websites.length;i++){
+            for (let i = 0; i < websites.length; i++) {
                 let tmp = document.createElement("button");
                 tmp.innerText = websites[i]["website_name"]
                 tmp.name = websites[i]["web_address"];
                 tmp.value = websites[i]["website_id"];
                 console.log(tmp);
             }
-        }else{
+        } else {
             alert("Error")
         }
     }
+
     function something() {
-        var getWebsitesAjax = jQuery.ajax(url:"/scripts/ajax.php?get=websites", success: listWebsites() );
+        $.ajax({
+            type: "GET",
+            url: "scripts/ajax.php",
+            data: "get=websites",
+            dataType: "JSON",
+            success: function(response) {
+
+            }
+        });
     }
 
     something();
