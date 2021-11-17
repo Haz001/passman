57039613a9096dbebb20c764a594800d48fbb17f
Haz <haz001@hazonline.co.uk>
2021-11-17 16:19:56 +0000

    js update

--- public_html/menu.php
+++ public_html/menu.php
@@ -1,37 +1,36 @@
 <?php require_once "header.php";
 //
 ?>
-<div id="list"><button onclick="listWebsites(this.parent)">Refresh</button> </div>
+<div id="list"><button id="refresh">Refresh</button> </div>
 <script>
-    function listWebsites(result) {
-        if (result.status == 200) {
-            let websitesJson = result.responseText;
-            let websites = JSON.parse(websitesJson);
-            console.log(websites);
-            for (let i = 0; i < websites.length; i++) {
-                let tmp = document.createElement("button");
-                tmp.innerText = websites[i]["website_name"]
-                tmp.name = websites[i]["web_address"];
-                tmp.value = websites[i]["website_id"];
-                console.log(tmp);
-            }
-        } else {
-            alert("Error")
-        }
-    }
+	function listWebsites(div) {
+		$.ajax({
+			type: "GET",
+			url: "/scripts/ajax.php",
+			data: "get=websites",
+			dataType: "JSON",
+			success: function (response) {
+				if (result.status == 200) {
+					let websitesJson = result.responseText;
+					let websites = JSON.parse(websitesJson);
+					console.log(websites);
+					for (let i = 0; i < websites.length; i++) {
+						let tmp = document.createElement("button");
+						tmp.innerText = websites[i]["website_name"]
+						tmp.name = websites[i]["web_address"];
+						tmp.value = websites[i]["website_id"];
+						console.log(tmp);
+					}
+				} else {
+					alert("Error")
+				}
+			}
+		});
+	}
+    $("#refresh").click(function (e) { 
+        e.preventDefault();
+        
+    });
 
-    function something() {
-        $.ajax({
-            type: "GET",
-            url: "scripts/ajax.php",
-            data: "get=websites",
-            dataType: "JSON",
-            success: function(response) {
-
-            }
-        });
-    }
-
-    something();
 </script>
 <?php require "footer.php"; ?>
