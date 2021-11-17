333fa18b00090f2ee31bdeb511d176d2016c0066
Haz <haz001@hazonline.co.uk>
2021-11-17 06:07:23 +0000

    FUCK JAVASCRIPT!!!

--- public_html/menu.php
+++ public_html/menu.php
@@ -1,10 +1,27 @@
-<?php //require_once "header.php";
+<?php require_once "header.php";
 //?>
-<?php
-session_start(["cookie_domain" => "passman.harrysy.red"]);
-$uid = $_SESSION["user_id"];
-$key = $_COOKIE["key"];
+    <div id="list"><button onclick="listWebsites(this.parent)" >Refresh</button> </div>
+<script>
+    function listWebsites(result){
+        if(result.status == 200) {
+            let websitesJson = result.responseText;
+            let websites = JSON.parse(websitesJson);
+            console.log(websites);
+            for (let i = 0;i < websites.length;i++){
+                let tmp = document.createElement("button");
+                tmp.innerText = websites[i]["website_name"]
+                tmp.name = websites[i]["web_address"];
+                tmp.value = websites[i]["website_id"];
+                console.log(tmp);
+            }
+        }else{
+            alert("Error")
+        }
+    }
+    function something() {
+        var getWebsitesAjax = jQuery.ajax(url:"/scripts/ajax.php?get=websites", success: listWebsites() );
+    }
 
-
-?>
+    something();
+</script>
 <?php require "footer.php"; ?>
