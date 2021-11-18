52aac07aaeff46948dfefb931d3bc9f8276aee98
Khal-R <91947633+Khal-R@users.noreply.github.com>
2021-11-18 14:24:51 +0000

    meh css

---
+++ public_html/reportabug.php
@@ -0,0 +1,20 @@
+<?php include 'header.php';?>
+
+<body>
+    <div class="containerReportBug">
+        <p id="NewBugReport">Report New Bug</p>
+
+        <form>
+            <input type="text" name="title">
+			<input type="text" name="frequency">
+            <input type="text" name="serverity">
+			<input type="text" name="description">
+			<button type="submit" name="submit">Submit</button>
+        </form>
+    </div>    
+</body>
+
+
+
+
+<?php include 'footer.php';?>
