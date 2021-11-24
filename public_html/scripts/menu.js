48de2111f6e0ac0ccdedc2912dcf420ef6570d76
Haz <haz001@hazonline.co.uk>
2021-11-24 03:16:05 +0000

    added edit button but only enables fileds.

--- public_html/scripts/menu.js
+++ public_html/scripts/menu.js
@@ -34,6 +34,18 @@ function listWebsites(div) {
 	});
 }
 
+function editPasswords(btn) {
+	console.log(btn.name);
+	let tmp = $('[name="'+btn.name+'"]');
+	for (let i = 0;i < tmp.length;i++)
+	{
+		if (tmp[i].type == "text")
+		{
+			tmp[i].disabled = false;
+		}
+	}
+	btn.value="Save";
+}
 function grabPasswords(btn) {
 	let div = $("#passwords");
 	console.log(btn);
@@ -50,24 +62,50 @@ function grabPasswords(btn) {
 				alert("PHP code error");
 			},
 			401: function () {
-				alert("please authenticate");
 				document.location = "/logout.php";
 			},
 		},
 		success: function (response) {
 			div.empty();
+			let tmp0 = document.createElement("h1");
+			tmp0.innerText = btn.innerText;
+			tmp0.name = btn.name;
+			let tmp1 = document.createElement("h2");
+			tmp1.innerText = btn.name;
+			div.append(tmp0);
+			div.append(tmp1);
 			for (let i = 0; i < response.length; i++) {
-				let tmp = document.createElement("input");
-				tmp.type = "text";
-				tmp.name = response[i]["password_id"];
-				tmp.value = response[i]["username"];
+				tmpName = "password_id:"+response[i]["password_id"];
+				if(i>0){
+					let hr = document.createElement("hr");
+					div.append(hr);
+				}
+				let br = document.createElement("br");
+				let tmp0 = document.createElement("h1");
+				tmp0.innerText = "Account "+i.toString()+":";
+				let tmp1 = document.createElement("input");
+				tmp1.type = "text";
+				tmp1.disabled = true;
+				tmp1.name = tmpName;
+				tmp1.value = response[i]["username"];
 				let tmp2 = document.createElement("input");
 				tmp2.type = "text";
-				tmp2.name = response[i]["password_id"];
+				tmp2.disabled = true;
+				tmp2.name = tmpName;
 				tmp2.value = response[i]["password"];
-				console.log(tmp);
-				div.append(tmp);
+				let tmp3 = document.createElement("input");
+				tmp3.type = "button";
+				tmp3.name = tmpName;
+				tmp3.value = "Edit";
+				tmp3.addEventListener("click", function () {
+					editPasswords(tmp3);
+				});
+				div.append(tmp0);
+				div.append(tmp1);
+				div.append(br);
 				div.append(tmp2);
+				div.append(br);
+				div.append(tmp3);
 			}
 		},
 	});
