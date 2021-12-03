4afac0670a14297d67165962e9b58b6336b16fb6
BenForino <benforino@gmail.com>
2021-12-03 12:37:24 +0000

    Styles!

--- public_html/header.php
+++ public_html/header.php
@@ -8,7 +8,7 @@ session_start(["cookie_domain" => "passman.harrysy.red"]);
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="/styles/cssOskar.css">
+	<link rel="stylesheet" href="/styles/styles.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="scripts/userInfo.js"></script>
 	<script src="scripts/errorHandle.js"></script>
