8a0ccd6e04429534efd9d6c3d9d2fd96095db91a
BenForino <benforino@gmail.com>
2021-12-06 13:06:56 +0000

    Simplifiying Error Script

--- public_html/scripts/errorHandle.js
+++ public_html/scripts/errorHandle.js
@@ -20,69 +20,55 @@ $(document).ready(function () {
 function errorMsg($_GET) {
 	switch ($_GET["error"]) {
 		case "ef":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
+			displayErrorMsg(
 				"There are empty fields, please check your details and try again"
 			);
 			break;
 		case "notfound":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
 			$("input").css("border-color", "red");
-			$("#error").html(
+			displayErrorMsg(
 				"Username/Password inccorect, please check your details and try again"
 			);
 			break;
 		case "stmtfailed":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
+			displayErrorMsg(
 				"Database error, please try again in a few minutes, if issue persists, contact support"
 			);
 			break;
 		case "otpExpired":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
+			displayErrorMsg(
 				"Your one time passcode has expired, please login again, otp's expire after 20 minutes"
 			);
 			break;
 		case "maxlength":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html("Max input length exceeded");
+			displayErrorMsg("Max input length exceeded");
 			break;
 		case "complexity":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
+			displayErrorMsg(
 				'Password does not meet complexity requirements <br> <ul style="list-style-type: none"><li>8 Characters</li><li>1 Special Character</li><li>1 Uppercase Character</li><li>1 Number</li><li>Not be in top 100,000 most common passwords</ul>'
 			);
 			$("[name='password']").css("border-color", "red");
 			break;
 		case "otpIncorrect":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
-				"One time passcode is incorrect, please try again"
-			);
+			displayErrorMsg("One time passcode is incorrect, please try again");
 			break;
 		case "ue":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
+			displayErrorMsg(
 				"A user with with your username or email already exists, please try <a href='index.php'>logging in</a>"
 			);
 			break;
 		case "commonPassword":
-			$("#error").css("background", "lightcoral");
-			$("#error").css("padding", "10px");
-			$("#error").html(
+			displayErrorMsg(
 				"Your password has been found in a common list of passwords, please try a more complex password"
 			);
 			break;
 		default:
 			break;
 	}
+}
+
+function displayErrorMsg(message) {
+	$("#error").css("background", "lightcoral");
+	$("#error").css("padding", "10px");
+	$("#error").html(message);
 }
