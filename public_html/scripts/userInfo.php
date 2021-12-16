7d8b4c004054ee02beb461081a462004fb27a396
BenForino <benforino@gmail.com>
2021-12-16 20:29:17 +0000

    Comments

--- public_html/scripts/userInfo.php
+++ public_html/scripts/userInfo.php
@@ -1,7 +1,7 @@
 <?php session_start(["cookie_domain" => "passman.harrysy.red"]);
 require_once "db.php";
 if (isset($_SESSION["user_id"])) {
-	$sql = "SELECT first_name, last_name FROM user WHERE user_id = ?";
+	$sql = "SELECT first_name, last_name FROM user WHERE user_id = ?"; //Selects the user details required and returns them in a JSON format
 	$stmt = mysqli_stmt_init($conn);
 	mysqli_stmt_prepare($stmt, $sql);
 	mysqli_stmt_bind_param($stmt, "i", $_SESSION["user_id"]);
